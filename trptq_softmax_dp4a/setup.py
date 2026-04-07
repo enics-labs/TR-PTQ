53a0521fda842f0fc2aadb265abce94a84738586
eliyahudev <eliyahulevyy@gmail.com>
2026-04-07 13:55:52 +0300

    fixing ImportError: libc10.so error

--- trptq_softmax_dp4a/setup.py
+++ trptq_softmax_dp4a/setup.py
@@ -1,5 +1,14 @@
+import os
+
+import torch
 from setuptools import setup
 from torch.utils.cpp_extension import BuildExtension, CUDAExtension
+
+TORCH_LIB_DIR = os.path.join(os.path.dirname(torch.__file__), 'lib')
+
+extra_link_args = []
+if os.name != 'nt':
+    extra_link_args.append(f'-Wl,-rpath,{TORCH_LIB_DIR}')
 
 setup(
     name='trptq_softmax_dp4a',
@@ -10,10 +19,13 @@ setup(
                 'softmax_cuda.cpp',
                 'softmax_cuda_kernel.cu',
             ],
+            library_dirs=[TORCH_LIB_DIR],
+            runtime_library_dirs=[TORCH_LIB_DIR] if os.name != 'nt' else [],
             extra_compile_args={
                 'cxx': ['-O3'],
                 'nvcc': ['-O3']
-            }
+            },
+            extra_link_args=extra_link_args,
         )
     ],
     cmdclass={'build_ext': BuildExtension}
